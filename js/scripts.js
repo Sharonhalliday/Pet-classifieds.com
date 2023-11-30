d20155134c07af29f6ed5dc4ecfcf4ad18e6bf67
sharonhalliday <hallidaysharon5@gmail.com>
2023-11-29 23:53:13 -0800

    add javaScript

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+$("#dark").click(function () {
+$("body").removeClass();
+$("body").addClass("first");
+})
+
+$("#white").click(function () {
+$("body").removeClass();
+$("body").addClass("second");
+})
+
+
+})
