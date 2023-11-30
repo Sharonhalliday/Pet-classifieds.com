040872c2d4b90156decb6f82916367f376acdd56
sharonhalliday <hallidaysharon5@gmail.com>
2023-11-30 12:46:17 -0800

    add enhance

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function() {  
+
+
 $("#dark").click(function () {
 $("body").removeClass();
 $("body").addClass("first");
-})
+});
 
 $("#white").click(function () {
 $("body").removeClass();
 $("body").addClass("second");
-})
+});
 
+$("p#highlight").click(function () {
+$("p").removeClass("enhance");
+$("p").addClass("enhance");
+});
 
-})
+});
